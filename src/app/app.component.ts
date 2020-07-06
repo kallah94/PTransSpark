dfb74731338d6939b59620f93a6f18fc9f70383f
devtool <devtool@localhost.localdomain>
2020-07-06 03:07:30 -0500

    final sidebarAdmin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,54 @@
 ﻿import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import  { faAtom, faUserAlt, faSignInAlt, faSignOutAlt, faRegistered, faUserPlus, faHome } from '@fortawesome/free-solid-svg-icons';
+import * as fa from '@fortawesome/free-solid-svg-icons';
 
 import { AuthenticationService } from './_services';
 import { User } from './_models';
 
 import './_content/app.less';
 
-@Component({ 
- selector: 'app',
- templateUrl: 'app.component.html',
- styleUrls: ['app.component.scss']
- })
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
 
 export class AppComponent {
-    currentUser: User;
-     _opened: boolean = false;
-     isAdmin: boolean = false;
-    faatom = faAtom
-    fahome = faHome
-    falogin = faSignInAlt
-    falogout = faSignOutAlt
-    faregister = faUserPlus
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => {this.currentUser = x;
-        const admins = ['kallah', 'mbagnick', 'olimata']
-        if(this.currentUser){
-            this.isAdmin = admins.includes(this.currentUser.username)
-        }
-    });
-    }
+  currentUser: User;
+  _opened = false;
+  isAdmin = false;
+  faatom = fa.faAtom;
+  fahome = fa.faHome;
+  falogin = fa.faSignInAlt;
+  falogout = fa.faSignOutAlt;
+  faregister = fa.faUserPlus;
+  fasession = fa.faPlusCircle;
+  faupdate = fa.faCogs;
+  fadelete = fa.faTrash;
+  fasess = fa.faRoad;
+  falist = fa.faList;
+  fastatement = fa.faShareAlt;
 
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
-
-    public _toggleSidebar() {
-        this._opened = !this._opened;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => {
+    this.currentUser = x;
+      const admins = ['kallah', 'mbagnick', 'olimata']
+      if (this.currentUser) {
+        this.isAdmin = admins.includes(this.currentUser.username)
       }
+    });
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  public _toggleSidebar() {
+    this._opened = !this._opened;
+  }
 
 }
